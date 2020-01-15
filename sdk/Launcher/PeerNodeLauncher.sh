4c073756a2f014f900692e4ead54807c5097a8f7
mengxk <xiaokun.meng@qcast.cn>
2020-01-15 16:18:49 +0800

    update sdk/Launcher/PeerNodeLauncher.sh.

--- sdk/Launcher/PeerNodeLauncher.sh
+++ sdk/Launcher/PeerNodeLauncher.sh
@@ -6,6 +6,7 @@ set -o errexit
 CURRENT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd);
 LIB_DIR="$(dirname $CURRENT_DIR)/lib";
 
+export PATH="$CURRENT_DIR:$PATH";
 export LD_LIBRARY_PATH="$LIB_DIR";
-"$CURRENT_DIR"/PeerNodeLauncher $@;
+PeerNodeLauncher $@;
 
